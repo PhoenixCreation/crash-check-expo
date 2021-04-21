52c2de2213064f7b6e56ab7189ce62259d14bef7
PhoenixCreation <phs28052001@gmail.com>
2021-04-21 12:40:06 +0530

    changes.

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const randomWidth = useSharedValue(10);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+      height: 50,
+      backgroundColor: "red",
+      marginBottom: 20,
+    };
+  });
   return (
     <View style={styles.container}>
-      <Text>This is blank canvas.</Text>
+      <Animated.View style={style}></Animated.View>
+      <Button
+        onPress={() => (randomWidth.value = Math.random() * 350)}
+        title="toggle"
+      />
       <StatusBar style="auto" />
     </View>
   );
